03449e28e451cca79cef745e8e9c2b489b69dd1f
Tsukasa Arima <pyar6329@gmail.com>
2016-10-09 16:36:26 +0900

    add front config

--- front/builds/development/node/docker-entrypoint.sh
+++ front/builds/development/node/docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ "$1" = "sh" -o "$1" = "bash" ]; then
     exec "bash"
 fi
 
-if [ "$1" = "node" -o "$2" = "node" ]; then
-    cd /usr/src/app
+if [ "$1" = "npm" -o "$2" = "start" ]; then
+    cd /app
     exec "$@"
 fi
